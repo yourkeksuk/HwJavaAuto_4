d41ffe7781dde07de4b96b7473fd1159140fac65
yourkeksuk <igor-18css@mail.ru>
2022-10-17 16:47:20 +0300

    Test commit

---
+++ src/test/java/FormTest.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.security.Key;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FormTest {
+
+    String generateDate (int date){
+        return LocalDate.now().plusDays(date).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void formTest (){
+        String date = generateDate(3);
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL,Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Пупкин Василий");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $("[class=button__content]").click();
+        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на " + date), Duration.ofSeconds(15)).shouldBe(exist);
+    }
+}
